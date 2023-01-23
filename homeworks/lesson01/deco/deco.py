685d3dd303be96a7ae6380f78ecd6d0c6108b6f0
Michael Petroff <petroff.mb@gmail.com>
2023-01-23 19:00:05 +0500

    l01 deco start

--- homeworks/lesson01/deco/deco.py
+++ homeworks/lesson01/deco/deco.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+from collections import Counter
 
 
 def disable():
@@ -21,28 +22,53 @@ def decorator():
     return
 
 
-def countcalls():
+def countcalls(func):
     '''Decorator that counts calls made to the function decorated.'''
-    return
+    _counter = Counter()
+
+    def wrapper(*args):
+        _counter[func] += 1
+        return func(*args)
+
+    wrapper.calls = _counter[func]
+    return wrapper
 
 
-def memo():
+def memo(func):
     '''
     Memoize a function so that it caches all return values for
     faster future lookups.
     '''
-    return
+    _cache = {}
+
+    def wrapper(*args):
+        r = _cache.get(args, None)
+        if r is None:
+            r = func(*args)
+            _cache[args] = r
+        return r
+
+    return wrapper
 
 
-def n_ary():
+def n_ary(func):
     '''
     Given binary function f(x, y), return an n_ary function such
     that f(x, y, z) = f(x, f(y,z)), etc. Also allow f(x) = x.
     '''
-    return
+
+    def wrapper(*args):
+        if len(args) == 1:
+            return args[0]
+        if len(args) == 2:
+            return func(*args)
+        *s, x, y = args
+        return wrapper(*s, func(x, y))
+
+    return wrapper
 
 
-def trace():
+def trace(func):
     '''Trace calls made to function decorated.
 
     @trace("____")
@@ -62,8 +88,11 @@ def trace():
      <-- fib(3) == 3
 
     '''
-    return
 
+    def wrapper(*args):
+        return func(*args)
+
+    return wrapper
 
 @memo
 @countcalls
@@ -79,12 +108,12 @@ def bar(a, b):
     return a * b
 
 
-@countcalls
-@trace("####")
-@memo
-def fib(n):
-    """Some doc"""
-    return 1 if n <= 1 else fib(n-1) + fib(n-2)
+# @countcalls
+# @trace("####")
+# @memo
+# def fib(n):
+#     """Some doc"""
+#     return 1 if n <= 1 else fib(n-1) + fib(n-2)
 
 
 def main():
@@ -97,10 +126,10 @@ def main():
     print(bar(4, 3, 2))
     print(bar(4, 3, 2, 1))
     print("bar was called", bar.calls, "times")
-
-    print(fib.__doc__)
-    fib(3)
-    print(fib.calls, 'calls made')
+    print()
+    # print(fib.__doc__)
+    # fib(3)
+    # print(fib.calls, 'calls made')
 
 
 if __name__ == '__main__':
